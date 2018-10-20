c5a2436e607582e141844c19341a3d47a678b9ef
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-10-20 17:20:07 +0200

    initial commit

---
+++ blockchain/src/main.rs
@@ -0,0 +1,56 @@
+use std::time::{SystemTime};
+extern crate sha2;
+
+fn main() {
+    let mut blockchain = Blockchain::default();
+
+    blockchain.add(String::from("my data"));
+
+    for i in blockchain.blocks.iter() {
+        // TODO: how to print the time
+        println!("data: {} | hash: {} | previous: {} | time: {}", i.data, i.hash, i.prev_hash, "idk");
+    }
+
+     match blockchain.last() {
+        None => println!("none"),
+        Some(last) => {
+            println!("{}", last.hash)
+        },
+     }
+}
+
+struct Block {
+    data: String,
+    hash: String,
+    prev_hash: String,
+    time: SystemTime,
+}
+
+struct Blockchain {
+    blocks: Vec<Block>,
+}
+
+impl Default for Blockchain {
+    fn default() -> Blockchain {
+        Blockchain {
+            blocks: Vec::new(),
+        }
+    }
+}
+
+
+impl Blockchain {
+    fn add(&mut self, data: String) {
+        // TODO: calculate hash
+        if self.blocks.len() == 0 {
+            // TODO: no previous hash when genesis block
+        }
+        let block = Block{data: data, hash: String::from("cur hash"), prev_hash: String::from("prev hash"), time: SystemTime::now()};
+        self.blocks.append(&mut vec![block])// TODO: Doesn't work
+    }
+
+    fn last(&self) -> Option<&Block> {
+        return self.blocks.last();
+    }
+}
+
