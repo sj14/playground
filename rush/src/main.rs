fe854ff4f9168aae77cf9133cea71d97c83bf5a5
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2019-03-19 22:32:07 +0100

    Execute commands directly

--- rush/src/main.rs
+++ rush/src/main.rs
@@ -3,25 +3,30 @@ use std::process::Command;
 use std::str;
 
 fn main() {
-    let mut line = String::new();
-    let input = io::stdin();
+    loop {
+        print!("> ");
+        io::stdout().flush().ok().expect("Could not flush stdout");
 
-    print!("> ");
-    io::stdout().flush().ok().expect("Could not flush stdout");
+        let mut line = String::new();
+        let input = io::stdin();
+        input
+            .lock()
+            .read_line(&mut line)
+            .expect("Could not read stdin");
 
-    input
-        .lock()
-        .read_line(&mut line)
-        .expect("Could not read stdin");
+        let args = line.split_whitespace().collect::<Vec<&str>>();
 
-    // TODO: don't use sh
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(line)
+        if args.len() < 1 {
+            continue;
+        }
+
+        let output = Command::new(args[0])
+        .arg(args[1]) // TODO: use all given args
         .output()
         .expect("Could not execute process");
 
-    let s = str::from_utf8(&output.stdout).expect("Could not convert output to string");
+        let s = str::from_utf8(&output.stdout).expect("Could not convert output to string");
 
-    println!("output: {}", s.trim_right());
+        println!("output: {}", s.trim_right());
+    }
 }
