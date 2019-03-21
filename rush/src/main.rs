34ebd21ba465a3852ae8c4a621134f298a3cc966
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2019-03-21 09:38:02 +0100

    Fix command arguments

--- rush/src/main.rs
+++ rush/src/main.rs
@@ -14,17 +14,15 @@ fn main() {
             .read_line(&mut line)
             .expect("Could not read stdin");
 
-        let args = line.split_whitespace().collect::<Vec<&str>>();
+        let mut args = line.split_whitespace().collect::<Vec<&str>>();
 
         if args.len() < 1 {
             continue;
         }
 
         let mut cmd = Command::new(args[0]);
-        // TODO: remove first element
-        for a in args {
-            cmd.arg(a);
-        }
+        args.remove(0);
+        cmd.args(args);
 
         // TODO: Does not wait until the command has finished
         let output = match cmd.output() {
