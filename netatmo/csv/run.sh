03c063d48d8c7132f07dccdb8a80b35ce64fac0c
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2019-07-20 18:13:58 +0200

    Import downloaded data

--- netatmo/csv/run.sh
+++ netatmo/csv/run.sh
@@ -12,3 +12,5 @@ while [ $LAST_X_DAYS -gt 0 ]
     ./getmeasurecsv.sh $USER $PASS "70:ee:50:3f:03:d4" "02:00:00:05:ed:f2" "Rain" $DATE "RAIN"
     LAST_X_DAYS=$((LAST_X_DAYS-1))
 done
+
+python3 import_influxdb.py
