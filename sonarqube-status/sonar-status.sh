38cc43778a638d939ad2084a74eaf6fda0bf8290
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2019-07-27 00:12:30 +0200

    Added sonar-status.sh

---
+++ sonarqube-status/sonar-status.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Description
+# ===========
+# Get the SonarQube Quality Guard status of a specific project for the current git commit.
+# When the Quality Guard analysis is "OK", return 0, otherwise, return 1.
+# Well suitable for usage in a build pipeline.
+#
+# Requirements
+# ============
+# git, curl, jq
+#
+# Usage
+# =====
+# 1. Modify the SONAR_HOST variable
+# 2. Source this file (`source sonar-status.sh`)
+# 3. Call `sonar_analysis_status YOUR_PROJECT_KEY`
+# 4. Check for the return code.
+#
+# Alternative
+# ===========
+# 1. Modify the SONAR_HOST variable
+# 2. Add `sonar_analysis_status YOUR_PROJECT_KEY` at the bottom of this file
+# 3. Execute this script `./sonar-status.sh`` 
+# 4. Check for the return code.
+
+set -e
+
+SONAR_HOST="htts://YOUR-SONARQUBE-SERVER/"
+GIT_COMMIT=$(git rev-parse HEAD)
+
+sonar_analysis_status() {
+    project_key="$1"
+
+    retry=0
+    while [  $retry -lt 6 ]; do
+        # get latest analysis id to corresponding git commit hash
+        analysis_id=$(curl -s $SONAR_HOST/api/project_analyses/search?project="$project_key" | jq '.analyses[] | select(.revision == "'"$GIT_COMMIT"'") | .key' | head -n 1)
+
+        # remove double quotes from analysis id (e.g. "AWv6wb07Y5FuS8wxa-xk" -> AWv6wb07Y5FuS8wxa-xk)
+        analysis_id=$(echo "$analysis_id" | tr -d "\"\`'")
+
+        # get quality guard status of this anlysis
+        analysis_status=$(curl -s $SONAR_HOST/api/qualitygates/project_status?analysisId="$analysis_id" | jq '.projectStatus.status')
+
+        # break this loop when analysis was found,
+        # otherwise, try again (next loop iteration)
+        if [ "$analysis_status" != null ]
+            then
+            break
+        fi
+
+        echo "Retry $retry: $project_key"
+        (( retry=retry+1 ))
+        
+        sleep 10s
+    done
+
+    # print and return status depending on result
+    printf "%s \t %-35s \t $SONAR_HOST/dashboard?id=%s\n" "$analysis_status" "$project_key" "$project_key"
+    if [ "$analysis_status" = \""OK\"" ]
+        then
+            return 0
+        else
+            return 1
+    fi
+}
+
+# Alternative (uncomment the following line)
+# sonar_analysis_status YOUR_PROJECT_KEY
