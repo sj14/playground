f5a43e50b678a76621f957fdc418fbe4fc6f5c2f
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2020-03-14 12:38:53 +0100

    signature TODO

--- ffmpeg-mac-installer/install.sh
+++ ffmpeg-mac-installer/install.sh
@@ -16,10 +16,20 @@ fi
 
 printf "downloading... "
 curl $dlURL -JL -s -o ffmpeg.zip
+
+# TODO: check signature
+# printf "verify signature... "
+# curl "$dlURL/sig" -JL -s -o ffmpeg.zip.sig
+# gpg --verify ffmpeg.zip.sig ffmpeg.zip
+
 printf "extracting... "
 unzip -q ffmpeg.zip
+
 printf "installing... "
 mv ffmpeg /usr/local/bin/
+
 printf "cleaning up... "
 rm ffmpeg.zip
+rm ffmpeg.zip.sig
+
 printf "finished!\n"
