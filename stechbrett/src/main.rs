9e5f183b98397a491da3988a0e104df70a76549c
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2020-03-18 21:33:01 +0100

    Use a function

--- stechbrett/src/main.rs
+++ stechbrett/src/main.rs
@@ -3,57 +3,51 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
-    let lottery_ticket = rand::thread_rng().gen_range(1, 1201);
+    let ticket = rand::thread_rng().gen_range(1, 1201);
     // let lottery_ticket = 666;
-    println!("ticket: {}", lottery_ticket);
+    println!("ticket: {}", ticket);
+    println!("{}", lottery(ticket));
+}
 
-    match lottery_ticket {
+fn lottery<'a>(ticket: i32) -> &'a str {
+    match ticket {
         1100 => {
-            println!("magic number! You win 100 €");
-            return;
+            return "magic number! You win 100 €";
         }
         270 => {
-            println!("magic number! You win 70 €");
-            return;
+            return "magic number! You win 70 €";
         }
         750 => {
-            println!("magic number! You win 50 €");
-            return;
+            return "magic number! You win 50 €";
         }
         340 | 540 | 840 => {
-            println!("magic number! You win 40 €");
-            return;
+            return "magic number! You win 40 €";
         }
         230 | 630 | 1030 => {
-            println!("magic number! You win 30 €");
-            return;
+            return "magic number! You win 30 €";
         }
         120 | 420 | 720 => {
-            println!("magic number! You win 20 €");
-            return;
+            return "magic number! You win 20 €";
         }
-
         _ => (),
     }
 
-    if lottery_ticket % 100 == 0 && lottery_ticket < 1000 {
-        println!("modulo 100! You win 10 €!");
-        return;
+    if ticket % 100 == 0 && ticket < 1000 {
+        return "modulo 100! You win 10 €!";
     }
 
-    let s = lottery_ticket.to_string();
+    let s = ticket.to_string();
 
-    if lottery_ticket >= 100
-        && lottery_ticket <= 1000
+    if ticket >= 100
+        && ticket <= 1000
         && s.chars().nth(0) == s.chars().nth(1)
         && s.chars().nth(1) == s.chars().nth(2)
     {
-        println!("repdigit! You win 5 €!");
-        return;
+        return "repdigit! You win 5 €!";
     }
 
     if s.chars().nth(s.len() - 1).unwrap() == '7' {
-        println!("last digit 7! You get a free ticket!");
-        return;
+        return "last digit 7! You get a free ticket!";
     }
+    return "nothing :(";
 }
