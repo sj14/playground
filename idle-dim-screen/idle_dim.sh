2c41b1429fc2e51b347d43a4018041d6edfb429c
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2020-05-14 19:14:02 +0200

    Remove old code

--- idle-dim-screen/idle_dim.sh
+++ idle-dim-screen/idle_dim.sh
@@ -5,10 +5,7 @@
 
 checkInterval=1   # seconds
 idleThreshold=20   # seconds
-idleBrightness=0   # percent
-wakeBrightness=0.5 # percent
 state="bright"
-lowered=0
 
 while :
 do
@@ -17,34 +14,21 @@ do
 
     if  [[ "$idleSec" -gt $idleThreshold ]] ; then
         if [ $state != "dark" ] ; then
-          # idle since 60 seconds, dim display
-              echo "idling ($idleSec sec), dim display to $idleBrightness"
-             # brightness $idleBrightness
+              echo "idling ($idleSec sec), dim display"
 
-            # alternative:
-             # https://www.maketecheasier.com/adjust-screen-brightness-from-terminal-macos/
-            osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell' # repeat x times for dark
+            for i in {1..16}
+            do
+                osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'
+            done
 
-            ((lowered++))
-
-            if [ $lowered -ge 16 ]; then
-                state="dark"
-            fi
+            state="dark"
         fi
     else 
         if [ $state != "bright" ]; then
             echo "not idling ($idleSec sec)"
-            lowered=0
-            # we are not idle, check if screen is already on again
-            #brightnessLevel=$(brightness -l | awk 'END {print $NF}')
-              # if [[ $(echo "$brightnessLevel < 0.1" | bc) == 1 ]] ; then
-                 # echo "setting brightness to $wakeBrightness"
-                # screen is on but brightness level is below 10%, set it to 50%
-                # brightness $wakeBrightness
-
             for i in {1..12}
             do
-                 osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell' # repeat x times for dark
+                 osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'
             done
             state="bright"
         fi
