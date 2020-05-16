bf5c2197d2ef4a5208d20c3c07929e7c6367418f
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2020-05-16 09:47:43 +0200

    Add perfect_test()

--- fp-erlang/tail_recursion.erl
+++ fp-erlang/tail_recursion.erl
@@ -2,14 +2,24 @@
 
 -module(tail_recursion).
 -export([fib/1, perfect/1]).
+-export([perfect_test/0]).
 
 
 fib(N) -> fib(N, 0, 1).
 fib(0, A, _) -> A;
 fib(N, A, B) -> fib(N-1, A+B, A).
 
-
+perfect(0) -> false; % undefined!?
 perfect(N) -> perfect(N, 1, 0, 0). % N, counter, sum, remainder
 perfect(N, C, S, _) when C == N -> N == S;
 perfect(N, C, S, R) when R == 0 -> perfect(N, C+1, S+C, N rem (C+1));
 perfect(N, C, S, R) when R =/= 0 -> perfect(N, C+1, S, N rem (C+1)).
+
+perfect_test() ->
+    false = perfect(0),
+    false = perfect(1),
+    true = perfect(6),
+    true = perfect(28),
+    true = perfect(496),
+    false = perfect(497),
+    pass.
