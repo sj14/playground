7113d083ce62785bc6410bc33597b627f8011847
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2020-05-16 09:50:52 +0200

    Simplify

--- fp-erlang/tail_recursion.erl
+++ fp-erlang/tail_recursion.erl
@@ -10,10 +10,10 @@ fib(0, A, _) -> A;
 fib(N, A, B) -> fib(N-1, A+B, A).
 
 perfect(0) -> false; % undefined!?
-perfect(N) -> perfect(N, 1, 0, 0). % N, counter, sum, remainder
-perfect(N, C, S, _) when C == N -> N == S;
-perfect(N, C, S, R) when R == 0 -> perfect(N, C+1, S+C, N rem (C+1));
-perfect(N, C, S, R) when R =/= 0 -> perfect(N, C+1, S, N rem (C+1)).
+perfect(N) -> perfect(N, 1, 0). % N, counter, sum
+perfect(N, C, S) when C == N -> N == S;
+perfect(N, C, S) when N rem C == 0 -> perfect(N, C+1, S+C);
+perfect(N, C, S) -> perfect(N, C+1, S).
 
 perfect_test() ->
     false = perfect(0),
