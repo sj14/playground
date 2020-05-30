72b655f3267380c734d642d07fee22f48b297082
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2020-05-30 11:46:35 +0200

    wip: add_line

--- fp-erlang/chapter 3/assignment.erl
+++ fp-erlang/chapter 3/assignment.erl
@@ -26,7 +26,7 @@ add(W, N, R) ->
         true ->
             {entry, W, O} = get(W, R),      % get current element for current line numbers (O)
             RR = drop(W, R),                % drop current element from result
-            RR ++ [{entry, W, [{O, N}]}];   % add current element to result with old line numbers an new line number
+            RR ++ [{entry, W, add_line(N, O)}];   % add current element to result with old line numbers an new line number
         false -> R ++ [{entry, W, [{N}]}]     % add word as entry with line
     end.
 
@@ -34,19 +34,54 @@ add_test() ->
     T1 = add(my_word, 1, []),
     {entry,my_word,[{1}]} = get(my_word, T1),
 
-    T2 = add(my_word, 2, [{entry,my_word,1}]),
+    T2 = add(my_word, 2, [{entry,my_word,[{1}]}]),
     {entry,my_word,[{1,2}]} = get(my_word, T2),
 
-    T3 = add(my_word, 2, [{entry,not_my_word,6}, {entry,my_word,4}, {entry,abcd,3}]),
-    {entry,my_word,[{4,2}]} = get(my_word, T3),
+    T3 = add(my_word, 4, [{entry,not_my_word,[{6}]}, {entry,my_word,[{2}]}, {entry,abcd,[{3}]}]),
+    {entry,my_word,[{2,4}]} = get(my_word, T3),
+
+    T4 = add(my_word, 4, [{entry,not_my_word,[{6}]}, {entry,my_word,[{2,3}]}, {entry,abcd,[{3}]}]),
+    {entry,my_word,[{2,4}]} = get(my_word, T4),
+
     pass.
 
-%check if it's the same line (continue) or add different line
-% add_line(W, L, [R|Rs]) ->
-%     case {entry, W, O} = get(W, R) of 
-%         true -> same_line();
-%         false -> 
-%     end.
+add_line(L, C) ->
+    case tuple_size(hd(C)) == 2 of
+        true ->
+            {_, last} = hd(C),
+            [erlang:append_element(last,L)];
+        false -> [erlang:append_element(hd(C),L)]
+    end
+
+    % case hd(C) > 0 of % because idk how to do `hd(c) == {_}`
+    %     true -> 
+    %         % has only one line, no range so far
+    %         [erlang:append_element(hd(C),L)];
+    %     false ->
+    %         % should be a range now, e.g. {1,5}
+    %         {first, _} = C,
+    %         {first, L}
+    % end
+
+    % case is_list(c) of
+    %     true -> 
+
+    %         % we already have several lines
+    %         case C + 1 == L of
+    %             true -> 
+    %                 % new line follows last line
+    %                 {tl(C), L};
+    %             false -> 
+    %                 % new line more than 1 line after old line
+    %                 {C, L}
+    %         end;
+
+    %     false -> 
+    %         % only one line
+    %         [{C,L}]
+    % end
+.
+
 
 % drop word X from list of entries R.
 drop(_X, []) -> [];
