00653d807e96213ace2373535a2cfb31e83f45b9
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2020-05-31 14:19:46 +0200

    Add rock, paper, scissors

---
+++ fp-erlang/chapter 4/4.8 rock-paper-scissor/rps.erl
@@ -0,0 +1,73 @@
+-module(rps).
+
+-export([beat/1,
+         beat_test/0,
+         lose/1,
+         result/2,
+         result_test/0,
+         tournament/2,
+         tournament_test/0]).
+
+beat(X) ->
+    case X of
+        rock -> paper;
+        paper -> scissors;
+        scissors -> rock
+    end.
+
+beat_test() ->
+    paper = beat(rock),
+    scissors = beat(paper),
+    rock = beat(scissors),
+    pass.
+
+lose(X) ->
+    case X of
+        rock -> scissors;
+        scissors -> paper;
+        paper -> rock
+    end.
+
+result(X, X) -> draw;
+result(X, Y) ->
+    case lose(X) == Y of
+        true -> win;
+        _ -> lose
+    end.
+
+result_test() ->
+    lose = result(rock, paper),
+    win = result(rock, scissors),
+    draw = result(rock, rock),
+    draw = result(paper, paper),
+    lose = result(paper, scissors),
+    win = result(paper, rock),
+    win = result(scissors, paper),
+    draw = result(scissors, scissors),
+    lose = result(scissors, rock),
+    pass.
+
+tournament(L, R) ->
+    RR = lists:zipwith(fun (X, Y) -> result(X, Y) end,
+                       L,
+                       R),
+    lists:foldl(fun (X, Sum) ->
+                        case X of
+                            win -> Sum + 1;
+                            lose -> Sum - 1;
+                            draw -> Sum
+                        end
+                end,
+                0,
+                RR).
+
+tournament_test() ->
+    -1 = tournament([rock, rock, paper, paper],
+                    [rock, paper, scissors, rock]),
+    -2 = tournament([scissors, rock, paper, paper],
+                    [rock, paper, scissors, rock]),
+    0 = tournament([rock, rock, scissors, paper],
+                   [rock, paper, scissors, rock]),
+    2 = tournament([rock, scissors, scissors, paper],
+                   [rock, paper, scissors, rock]),
+    pass.
