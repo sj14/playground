3cd190d83d98ab6c8f61cd919f00f365a22431f3
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2021-03-05 17:06:24 +0100

    Simplify code

--- sonarqube-status/sonar-status.sh
+++ sonarqube-status/sonar-status.sh
@@ -12,31 +12,22 @@
 #
 # Usage
 # =====
-# 1. Modify the SONAR_HOST and AUTH_TOKEN variable
-# 2. Source this file (`source sonar-status.sh`)
-# 3. Call `sonar_analysis_status YOUR_PROJECT_KEY`
-# 4. Check for the return code.
-#
-# Alternative
-# ===========
-# 1. Modify the SONAR_HOST and AUTH_TOKEN variable
-# 2. Uncomment the last lines at the bottom of this file
-# 3. Execute this script `./sonar-status.sh`
-# 4. Check for the return code.
+# Execute this script `./sonar-status.sh <host> <auth_token> <project_key>` and check for the return code.
+# Example: `./sonar-status.sh http://localhost:9000 d7981b9c1333bef3ff83502cea6f6cd6ce4d70f6 test`
 
 set -euf -o pipefail
 
-SONAR_HOST="htts://YOUR-SONARQUBE-SERVER/"
+SONAR_HOST=$1
+AUTH_TOKEN=$2 # project key token or user token (see https://docs.sonarqube.org/latest/user-guide/user-token/)
+PROJECT_KEY=$3
+
 GIT_COMMIT=$(git rev-parse HEAD)
-AUTH_TOKEN="CHANGE_ME" # see https://docs.sonarqube.org/latest/user-guide/user-token/
 
 sonar_analysis_status() {
-    project_key="$1"
-
     retry=0
     while [  $retry -lt 6 ]; do
         # get latest analysis id to corresponding git commit hash
-        analysis_id=$(curl -s -u $AUTH_TOKEN: $SONAR_HOST/api/project_analyses/search?project="$project_key" | jq '.analyses[] | select(.revision == "'"$GIT_COMMIT"'") | .key' | head -n 1)
+        analysis_id=$(curl -s -u $AUTH_TOKEN: $SONAR_HOST/api/project_analyses/search?project="$PROJECT_KEY" | jq '.analyses[] | select(.revision == "'"$GIT_COMMIT"'") | .key' | head -n 1)
 
         # remove double quotes from analysis id (e.g. "AWv6wb07Y5FuS8wxa-xk" -> AWv6wb07Y5FuS8wxa-xk)
         analysis_id=$(echo "$analysis_id" | tr -d "\"\`'")
@@ -51,14 +42,14 @@ sonar_analysis_status() {
             break
         fi
 
-        echo "Retry $retry: $project_key"
+        echo "Retry $retry: $PROJECT_KEY"
         (( retry=retry+1 ))
         
         sleep 10s
     done
 
     # print and return status depending on result
-    printf "%s \t %-35s \t $SONAR_HOST/dashboard?id=%s\n" "$analysis_status" "$project_key" "$project_key"
+    printf "%s \t %-35s \t $SONAR_HOST/dashboard?id=%s\n" "$analysis_status" "$PROJECT_KEY" "$PROJECT_KEY"
     if [ "$analysis_status" = \""OK\"" ]
         then
             return 0
@@ -67,11 +58,9 @@ sonar_analysis_status() {
     fi
 }
 
-## Alternative:
-
-# if ! sonar_analysis_status YOUR_PROJECT_KEY
-# then
-#     exit 1
-# else
-#     exit 0
-# fi
+if ! sonar_analysis_status "$PROJECT_KEY"
+then
+    exit 1
+else
+    exit 0
+fi
