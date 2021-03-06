3da2e0d385fb4fc50afa8ccdd301587b8e0578d1
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2021-03-06 18:05:31 +0100

    Remove function

--- sonarqube-status/sonar-status.sh
+++ sonarqube-status/sonar-status.sh
@@ -23,44 +23,36 @@ PROJECT_KEY=$3
 
 GIT_COMMIT=$(git rev-parse HEAD)
 
-sonar_analysis_status() {
-    retry=0
-    while [  $retry -lt 6 ]; do
-        # get latest analysis id to corresponding git commit hash
-        analysis_id=$(curl -s -u $AUTH_TOKEN: $SONAR_HOST/api/project_analyses/search?project="$PROJECT_KEY" | jq '.analyses[] | select(.revision == "'"$GIT_COMMIT"'") | .key' | head -n 1)
 
-        # remove double quotes from analysis id (e.g. "AWv6wb07Y5FuS8wxa-xk" -> AWv6wb07Y5FuS8wxa-xk)
-        analysis_id=$(echo "$analysis_id" | tr -d "\"\`'")
+retry=0
+while [  $retry -lt 6 ]; do
+    # get latest analysis id to corresponding git commit hash
+    analysis_id=$(curl -s -u $AUTH_TOKEN: $SONAR_HOST/api/project_analyses/search?project="$PROJECT_KEY" | jq '.analyses[] | select(.revision == "'"$GIT_COMMIT"'") | .key' | head -n 1)
 
-        # get quality gate status of this anlysis
-        analysis_status=$(curl -s -u $AUTH_TOKEN: $SONAR_HOST/api/qualitygates/project_status\?analysisId="$analysis_id" | jq '.projectStatus.status')
+    # remove double quotes from analysis id (e.g. "AWv6wb07Y5FuS8wxa-xk" -> AWv6wb07Y5FuS8wxa-xk)
+    analysis_id=$(echo "$analysis_id" | tr -d "\"\`'")
 
-        # break this loop when analysis was found,
-        # otherwise, try again (next loop iteration)
-        if [ "$analysis_status" != null ]
-            then
-            break
-        fi
+    # get quality gate status of this anlysis
+    analysis_status=$(curl -s -u $AUTH_TOKEN: $SONAR_HOST/api/qualitygates/project_status\?analysisId="$analysis_id" | jq '.projectStatus.status')
 
-        echo "Retry $retry: $PROJECT_KEY"
-        (( retry=retry+1 ))
-        
-        sleep 10s
-    done
-
-    # print and return status depending on result
-    printf "%s \t %-35s \t $SONAR_HOST/dashboard?id=%s\n" "$analysis_status" "$PROJECT_KEY" "$PROJECT_KEY"
-    if [ "$analysis_status" = \""OK\"" ]
+    # break this loop when analysis was found,
+    # otherwise, try again (next loop iteration)
+    if [ "$analysis_status" != null ]
         then
-            return 0
-        else
-            return 1
+        break
     fi
-}
 
-if ! sonar_analysis_status
-then
-    exit 1
-else
-    exit 0
+    echo "Retry $retry: $PROJECT_KEY"
+    (( retry=retry+1 ))
+    
+    sleep 10s
+done
+
+# print and return status depending on result
+printf "%s \t %-35s \t $SONAR_HOST/dashboard?id=%s\n" "$analysis_status" "$PROJECT_KEY" "$PROJECT_KEY"
+if [ "$analysis_status" = \""OK\"" ]
+    then
+        exit 0
+    else
+        exit 1
 fi
