8d065bae038b2e19b568afbee0880c47f5046773
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2021-03-08 18:39:42 +0100

    Allowing changing comment symbol

--- dot-appender/src/main.rs
+++ dot-appender/src/main.rs
@@ -2,9 +2,10 @@ use serde_derive::Deserialize;
 use std::fs::OpenOptions;
 use std::io::Read;
 use std::io::Seek;
-use std::io::{Write};
+use std::io::Write;
 
 fn main() {
+    // TODO: read command line arguments (e.g. config filename or directory)
     read_appender_file();
     read_target_file();
 }
@@ -37,8 +38,13 @@ fn read_target_file() {
         toml::from_str(content.as_ref()).expect("faild reading toml file");
 
     for cfg in appender_configs.configs {
-        let header = format!("\n{} {}", cfg.comment, "DOT-APPENDER START\n");
-        let footer = format!("\n{} {}", cfg.comment, "DOT-APPENDER END\n");
+        println!("writing {}", cfg.target);
+
+        let boundary_start = "DOT-APPENDER START";
+        let boundary_stop = "DOT-APPENDER END";
+
+        let header = format!("\n{} {}\n", cfg.comment, boundary_start);
+        let footer = format!("\n{} {}\n", cfg.comment, boundary_stop);
         let mut file = OpenOptions::new()
             .write(true)
             .read(true)
@@ -50,18 +56,28 @@ fn read_target_file() {
         file.read_to_string(&mut content).expect("failed reading");
 
         // remove old appender config if exists
-        let start = content.find(&header).unwrap_or(content.len());
-        let mut end = content.find(&footer).unwrap_or(0);
-        if end != 0 {
-            end += footer.len();
-            content.replace_range(start..end, "");
+        let mut new_content = String::new();
+        let mut should_add = true;
+        for l in content.lines() {
+            if l.contains(boundary_start) {
+                new_content.pop(); // remove empty new line
+                should_add = false;
+            }
+            if l.contains(boundary_stop) {
+                should_add = true;
+                continue;
+            }
+
+            if should_add {
+                new_content.push_str(&format!("{}\n", l));
+            }
         }
 
         // overwrite file with new config
         // TODO: add backup of file
         file.seek(std::io::SeekFrom::Start(0))
             .expect("failed seeking");
-        file.write_all(content.as_bytes())
+        file.write_all(new_content.as_bytes())
             .expect("failed writing content");
         file.write_all(header.as_bytes()).expect("failed writing");
         file.write_all(cfg.append.as_bytes())
