d43e091e701c98e57d34adef3a99e0642bbcfa7e
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2022-01-29 21:58:04 +0100

    Rename variable

--- go-xread/xread.go
+++ go-xread/xread.go
@@ -2,25 +2,25 @@ package xread
 
 import "io"
 
-var err error
+var globalErr error
 
 // Read is an attempt for solving https://github.com/golang/go/issues/40385#issuecomment-663775687,
 // thus you can process the error before processing n.
 func Read(r io.Reader, p []byte) (int, error) {
-	if err != nil {
+	if globalErr != nil {
 		// Return the error from the previous call.
-		return 0, err
+		return 0, globalErr
 	}
 
 	var n int
-	n, err = r.Read(p)
+	n, globalErr = r.Read(p)
 	if n > 0 {
 		// Do not immediately return an error when bytes were read.
 		return n, nil
 	}
 
 	// No bytes were read, return whatever Read() returned.
-	return n, err
+	return n, globalErr
 }
 
 // ReadStrict is like Read() but always returns an io.EOF error when no bytes were read and there would be no error otherwise.
