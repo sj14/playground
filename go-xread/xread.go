66137ac5417f6b7d974b8312b44f64aa797ba71d
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2022-03-24 10:29:55 +0100

    Update xread to use struct instead of global variable

--- go-xread/xread.go
+++ go-xread/xread.go
@@ -2,30 +2,32 @@ package xread
 
 import "io"
 
-var globalErr error
+type Reader struct {
+	err error
+}
 
 // Read is an attempt for solving https://github.com/golang/go/issues/40385#issuecomment-663775687,
 // thus you can process the error before processing n.
-func Read(r io.Reader, p []byte) (int, error) {
-	if globalErr != nil {
+func (re *Reader) Read(r io.Reader, p []byte) (int, error) {
+	if re.err != nil {
 		// Return the error from the previous call.
-		return 0, globalErr
+		return 0, re.err
 	}
 
 	var n int
-	n, globalErr = r.Read(p)
+	n, re.err = r.Read(p)
 	if n > 0 {
 		// Do not immediately return an error when bytes were read.
 		return n, nil
 	}
 
 	// No bytes were read, return whatever Read() returned.
-	return n, globalErr
+	return n, re.err
 }
 
 // ReadStrict is like Read() but always returns an io.EOF error when no bytes were read and there would be no error otherwise.
-func ReadStrict(r io.Reader, p []byte) (int, error) {
-	n, err := Read(r, p)
+func (re *Reader) ReadStrict(r io.Reader, p []byte) (int, error) {
+	n, err := re.Read(r, p)
 	if n <= 0 && err == nil {
 		return n, io.EOF
 	}
---
+++ go-xread/xread_test.go
@@ -0,0 +1,50 @@
+package xread_test
+
+import (
+	"io"
+	"math/rand"
+	"testing"
+
+	"github.com/sj14/playground/go-xread"
+	"github.com/stretchr/testify/require"
+)
+
+type testReader struct {
+	calls         int
+	errAfterCalls int
+	errType       error
+}
+
+func (tr *testReader) Read(p []byte) (n int, err error) {
+	tr.calls++
+	if tr.calls > tr.errAfterCalls {
+		return rand.Intn(1), tr.errType
+	}
+	return rand.Int(), nil
+}
+
+func TestReadUnordered(t *testing.T) {
+	reader0 := &testReader{errAfterCalls: 3, errType: io.EOF}
+	reader1 := &testReader{errAfterCalls: 1, errType: io.ErrClosedPipe}
+
+	_, err := (&xread.Reader{}).Read(reader0, nil)
+	require.NoError(t, err)
+
+	_, err = (&xread.Reader{}).Read(reader1, nil)
+	require.NoError(t, err)
+
+	_, err = (&xread.Reader{}).Read(reader0, nil)
+	require.NoError(t, err)
+
+	_, err = (&xread.Reader{}).Read(reader1, nil)
+	require.EqualError(t, io.ErrClosedPipe, err.Error())
+
+	_, err = (&xread.Reader{}).Read(reader0, nil)
+	require.NoError(t, err)
+
+	_, err = (&xread.Reader{}).Read(reader1, nil)
+	require.EqualError(t, io.ErrClosedPipe, err.Error())
+
+	_, err = (&xread.Reader{}).Read(reader0, nil)
+	require.EqualError(t, io.EOF, err.Error())
+}
