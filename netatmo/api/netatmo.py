bd81d381fb398e0b24d2756d9ed3a99d2afc339e
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2024-06-05 18:07:00 +0200

    Fix deprecation warning

--- netatmo/api/netatmo.py
+++ netatmo/api/netatmo.py
@@ -86,7 +86,7 @@ while True:
                             CREATE INDEX IF NOT EXISTS idx_{0}_module ON {0} (module) STORING (value);
                             '''.format(sensor.lower(), valueType))
 
-                        cur.execute("INSERT INTO {0} (time, module, value) VALUES('{1}', '{2}', '{3}');".format(sensor.lower(), datetime.datetime.utcfromtimestamp(int(when)).strftime('%Y-%m-%d %H:%M:%S'), module.lower(), value))
+                        cur.execute("INSERT INTO {0} (time, module, value) VALUES('{1}', '{2}', '{3}');".format(sensor.lower(), datetime.datetime.fromtimestamp(int(when), datetime.UTC).strftime('%Y-%m-%d %H:%M:%S'), module.lower(), value))
                     except Exception as e:
                         print("failed postgres: {0}".format(e))
 
